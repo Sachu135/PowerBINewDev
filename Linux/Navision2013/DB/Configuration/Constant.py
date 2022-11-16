0648be70071a60ba6fbec901cd3843a5bba981ff
Sachu135 <sachin1.pandey@gmail.com>
2022-11-16 15:24:15 +0530

    PowerBI

--- Linux/Navision2013/DB/Configuration/Constant.py
+++ Linux/Navision2013/DB/Configuration/Constant.py
@@ -22,7 +22,7 @@ class ConfiguratorDbInfo:
     PostgresDB = "Configurator" 
     Schema = "#ConfiguratorDbInfo_Schema"              
     PostgresUrl = "jdbc:postgresql://" + Host + "/" + PostgresDB
-	props = {"user":"#PostgresDbInfo_User", "password":"#PostgresDbInfo_Password", "driver": "org.postgresql.Driver"}
+	props = {"user":"#ConfiguratorDbInfo_User", "password":"#ConfiguratorDbInfo_Password", "driver": "org.postgresql.Driver"}
 class ConnectionInfo:
     JDBC_PARAM = "jdbc"
     SQL_SERVER_DRIVER = "com.microsoft.sqlserver.jdbc.SQLServerDriver"
