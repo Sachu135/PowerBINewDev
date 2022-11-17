d7690d95a8f7a402a03c12a18df7bbb8e7b29052
Sachu135 <sachin1.pandey@gmail.com>
2022-11-17 21:38:15 +0530

    PowerBI

--- Linux/Navision2013/DB/Configuration/Constant.py
+++ Linux/Navision2013/DB/Configuration/Constant.py
@@ -7,7 +7,7 @@ owmode = 'overwrite'
 apmode = 'append'
 HDFS_PATH = "hdfs://master:9000"
 DIR_PATH="#ConfiguratorInstallationDrive"
-SPARK_MASTER = "spark://"+"#ConfiguratorDbInfo_ServerIP"+":7077"
+SPARK_MASTER = "spark://#ConfiguratorDbInfo_ServerIP:7077"
 class PostgresDbInfo:
     Host = "#PostgresDbInfo_Host"      
     Port = "#PostgresDbInfo_Port"   
